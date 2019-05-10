d9ce8d83b567321b85d86a6f4a3ce15129da0cec
ewewukek <ewewukek@gmail.com>
2019-05-10 13:42:25 +0300

    reload/fire cycle

--- src/main/java/ewewukek/musketmod/Musket.java
+++ src/main/java/ewewukek/musketmod/Musket.java
@@ -1,7 +1,16 @@
 package ewewukek.musketmod;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class Musket extends Item {
     public Musket() {
@@ -9,5 +18,41 @@ public class Musket extends Item {
             .defaultMaxDamage(250)
             .group(ItemGroup.COMBAT));
         setRegistryName(MusketMod.MODID, "musket");
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        ItemStack itemStack = playerIn.getHeldItem(handIn);
+        playerIn.setActiveHand(handIn);
+        return new ActionResult<>(EnumActionResult.SUCCESS, itemStack);
+    }
+
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft) {
+        if (!(entityLiving instanceof EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer)entityLiving;
+        if (isLoaded(stack)) {
+            System.out.println("PEW!");
+            stack.damageItem(1, player);
+            setLoaded(stack, false);
+        } else {
+            System.out.println("lock and load");
+            setLoaded(stack, true);
+        }
+    }
+
+    public int getUseDuration(ItemStack stack) {
+        return 72000;
+    }
+
+    public EnumAction getUseAction(ItemStack stack) {
+        return isLoaded(stack) ? EnumAction.BOW : EnumAction.BLOCK;
+    }
+
+    private void setLoaded(ItemStack stack, boolean loaded) {
+        stack.getOrCreateTag().setByte("loaded", (byte)(loaded ? 1 : 0));
+    }
+
+    private boolean isLoaded(ItemStack stack) {
+        NBTTagCompound tag = stack.getTag();
+        return tag != null && tag.getByte("loaded") == 1;
     }
 }

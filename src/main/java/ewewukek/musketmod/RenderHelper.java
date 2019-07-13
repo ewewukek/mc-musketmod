77c1572d287590eb2e4bc73db7ef774c1e03ab4d
ewewukek <ewewukek@gmail.com>
2019-07-14 01:12:10 +0300

    musket loading animation

--- src/main/java/ewewukek/musketmod/RenderHelper.java
+++ src/main/java/ewewukek/musketmod/RenderHelper.java
@@ -27,7 +27,30 @@ public class RenderHelper {
             GlStateManager.rotatef(180 + sign * (20 - 20 * swingSharp), 1, 0, 0);
 
         } else {
-            GlStateManager.translatef(sign * 0.15f, -0.27f + equipProgress * -0.6f, -0.37f);
+            float usingDuration = stack.getUseDuration() - (player.getItemInUseCount() - partialTicks + 1);
+            boolean isLoading = player.isHandActive() && player.getActiveHand() == hand && !MusketItem.isLoaded(stack)
+                                && usingDuration > 0 && usingDuration < MusketItem.RELOAD_DURATION;
+            if (isLoading) {
+                GlStateManager.translatef(sign * 0.15f, -0.55f, -0.3f);
+                GlStateManager.rotatef(60, 1, 0, 0);
+                GlStateManager.rotatef(10, 0, 0, 1);
+
+                if (usingDuration >= 8 && usingDuration <= 14 || usingDuration >= 18 && usingDuration <= 24) {
+                    if (usingDuration >= 18) usingDuration -= 10;
+                    float t;
+                    if (usingDuration < 10) {
+                        t = (usingDuration - 8) / 2;
+                        t = MathHelper.sin((float)Math.PI / 2 * MathHelper.sqrt(t));
+                    } else {
+                        t = (14 - usingDuration) / 4;
+                    }
+                    GlStateManager.translatef(0, 0, 0.02f * t);
+                }
+
+            } else {
+                equipProgress = 0;
+                GlStateManager.translatef(sign * 0.15f, -0.27f + equipProgress * -0.6f, -0.37f);
+            }
         }
 
         mc.getFirstPersonRenderer().renderItemSide(player, stack, isRightHand ? ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND : ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND, !isRightHand);

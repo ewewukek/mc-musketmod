fd048b016abb49017be44cb4d456d3cc19ee60ac
ewewukek <ewewukek@gmail.com>
2019-07-13 19:41:48 +0300

    first person musket position and swinging animation redone

--- src/main/java/ewewukek/musketmod/RenderHelper.java
+++ src/main/java/ewewukek/musketmod/RenderHelper.java
@@ -11,32 +11,26 @@ import net.minecraft.util.HandSide;
 import net.minecraft.util.math.MathHelper;
 
 public class RenderHelper {
-    // copies renderItemInFirstPerson
     public static void renderSpecificFirstPersonHand(Hand hand, float partialTicks, float interpolatedPitch, float swingProgress, float equipProgress, ItemStack stack) {
         Minecraft mc = Minecraft.getInstance();
         PlayerEntity player = mc.player;
         HandSide handside = hand == Hand.MAIN_HAND ? player.getPrimaryHand() : player.getPrimaryHand().opposite();
-        float xSign = handside == HandSide.RIGHT ? 1 : -1;
+        boolean isRightHand = handside == HandSide.RIGHT;
+        float sign = isRightHand ? 1 : -1;
 
         GlStateManager.pushMatrix();
 
-        float swingX = -0.4F * MathHelper.sin(MathHelper.sqrt(swingProgress) * (float)Math.PI);
-        float swingY = 0.2F * MathHelper.sin(MathHelper.sqrt(swingProgress) * ((float)Math.PI * 2F));
-        float swingZ = -0.2F * MathHelper.sin(swingProgress * (float)Math.PI);
-        GlStateManager.translatef(xSign * swingX, swingY, swingZ);
+        if (swingProgress > 0) {
+            float swingSharp = MathHelper.sin(MathHelper.sqrt(swingProgress) * (float)Math.PI);
+            float swingNormal = MathHelper.sin(swingProgress * (float)Math.PI);
+            GlStateManager.translatef(sign * (0.2f - 0.05f * swingNormal), -0.2f - 0.05f * swingNormal, -0.3f - 0.4f * swingSharp);
+            GlStateManager.rotatef(180 + sign * (20 - 20 * swingSharp), 1, 0, 0);
 
-        // transformSideFirstPerson
-        GlStateManager.translatef(xSign * 0.56F, -0.52F + equipProgress * -0.6F, -0.72F);
+        } else {
+            GlStateManager.translatef(sign * 0.15f, -0.27f + equipProgress * -0.6f, -0.37f);
+        }
 
-        // transformFirstPerson
-        float swingFactor = MathHelper.sin(swingProgress * swingProgress * (float)Math.PI);
-        GlStateManager.rotatef(xSign * (45.0F + swingFactor * -20.0F), 0.0F, 1.0F, 0.0F);
-        float swingRotation = MathHelper.sin(MathHelper.sqrt(swingProgress) * (float)Math.PI);
-        GlStateManager.rotatef(xSign * swingRotation * -20.0F, 0.0F, 0.0F, 1.0F);
-        GlStateManager.rotatef(swingRotation * -80.0F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.rotatef(xSign * -45.0F, 0.0F, 1.0F, 0.0F);
-
-        mc.getFirstPersonRenderer().renderItemSide(player, stack, handside == HandSide.RIGHT ? ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND : ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND, handside == HandSide.LEFT);
+        mc.getFirstPersonRenderer().renderItemSide(player, stack, isRightHand ? ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND : ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND, !isRightHand);
 
         GlStateManager.popMatrix();
      }

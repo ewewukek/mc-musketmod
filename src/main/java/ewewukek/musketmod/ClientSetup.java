81e0533fd7683065a206ec419e5a206d4354dfe4
ewewukek <ewewukek@gmail.com>
2021-08-25 08:07:33 +0300

    musket with bayonet

--- src/main/java/ewewukek/musketmod/ClientSetup.java
+++ src/main/java/ewewukek/musketmod/ClientSetup.java
@@ -1,13 +1,11 @@
 package ewewukek.musketmod;
 
-import static ewewukek.musketmod.MusketMod.MUSKET;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -23,15 +21,17 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientSetup {
     public static void init(final FMLClientSetupEvent event) {
-        ItemProperties.register(MUSKET, new ResourceLocation("loaded"), (stack, world, player, arg) -> {
+        ItemPropertyFunction loaded = (stack, world, player, arg) -> {
             return MusketItem.isLoaded(stack) ? 1 : 0;
-        });
+        };
+        ItemProperties.register(MusketMod.MUSKET, new ResourceLocation("loaded"), loaded);
+        ItemProperties.register(MusketMod.MUSKET_WITH_BAYONET, new ResourceLocation("loaded"), loaded);
     }
 
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void onRenderHandEvent(final RenderHandEvent event) {
         ItemStack stack = event.getItemStack();
-        if (!stack.isEmpty() && stack.getItem() == MUSKET) {
+        if (!stack.isEmpty() && stack.getItem() instanceof MusketItem) {
             Minecraft mc = Minecraft.getInstance();
             RenderHelper.renderSpecificFirstPersonHand(
                     mc.getItemInHandRenderer(), mc.player,
@@ -48,7 +48,7 @@ public class ClientSetup {
         Player player = (Player)event.getEntity();
         if (player.swinging) return;
         ItemStack stack = player.getMainHandItem();
-        if (!stack.isEmpty() && stack.getItem() == MUSKET && MusketItem.isLoaded(stack)) {
+        if (!stack.isEmpty() && stack.getItem() instanceof MusketItem && MusketItem.isLoaded(stack)) {
             PlayerModel<Player> model = event.getRenderer().getModel();
             if (player.getMainArm() == HumanoidArm.RIGHT) {
                 model.rightArmPose = HumanoidModel.ArmPose.CROSSBOW_HOLD;

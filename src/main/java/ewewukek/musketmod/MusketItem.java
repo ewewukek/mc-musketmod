615a80359910eb0d9a2e36b8a23cc0b48d7e07ec
ewewukek <ewewukek@gmail.com>
2021-08-26 05:28:15 +0300

    [fabric port] move bayonet damage constant to MusketItem

--- src/main/java/ewewukek/musketmod/MusketItem.java
+++ src/main/java/ewewukek/musketmod/MusketItem.java
@@ -13,24 +13,22 @@ import net.minecraft.world.item.Item;
 
 public class MusketItem extends GunItem {
     public static final int DURABILITY = 250;
+    public static final int BAYONET_DAMAGE = 3;
 
     public static float bulletStdDev;
     public static float bulletSpeed;
 
-    public final Multimap<Attribute, AttributeModifier> attributeModifiers;
+    public final boolean hasBayonet;
+    public final Multimap<Attribute, AttributeModifier> bayonetAttributeModifiers;
 
-    public MusketItem(Item.Properties properties) {
+    public MusketItem(Item.Properties properties, boolean withBayonet) {
         super(properties.defaultDurability(DURABILITY));
-        attributeModifiers = null;
-    }
-
-    public MusketItem(float attackDamage, Item.Properties properties) {
-        super(properties.defaultDurability(DURABILITY));
+        hasBayonet = withBayonet;
 
         Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
         builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(
-            BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", attackDamage, AttributeModifier.Operation.ADDITION));
-        attributeModifiers = builder.build();
+            BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", BAYONET_DAMAGE, AttributeModifier.Operation.ADDITION));
+        bayonetAttributeModifiers = builder.build();
     }
 
     @Override
@@ -50,8 +48,8 @@ public class MusketItem extends GunItem {
 
     @Override
     public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot slot) {
-        return slot == EquipmentSlot.MAINHAND && attributeModifiers != null
-                ? attributeModifiers
+        return slot == EquipmentSlot.MAINHAND && hasBayonet
+                ? bayonetAttributeModifiers
                 : super.getDefaultAttributeModifiers(slot);
     }
 }

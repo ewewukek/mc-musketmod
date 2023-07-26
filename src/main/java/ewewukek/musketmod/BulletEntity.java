2e2a6684bec2b92971739e0134a983737072229d
SkpC9 <zhixinzong@gmail.com>
2023-07-26 15:53:32 +0800

    fix "java.util.NoSuchElementException: No value present" caused by damage source

--- src/main/java/ewewukek/musketmod/BulletEntity.java
+++ src/main/java/ewewukek/musketmod/BulletEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
@@ -13,8 +14,11 @@ import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -60,9 +64,10 @@ public class BulletEntity extends AbstractHurtingProjectile {
         return tickCounter == 0;
     }
 
+    public static final ResourceKey<DamageType> MUSKET = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(MODID, "musket"));
+
     public DamageSource causeMusketDamage(BulletEntity bullet, Entity attacker) {
-       return new DamageSource(Holder.direct(new DamageType(MODID + ".musket", 0.5f))
-                ,bullet, attacker);
+        return level().damageSources().source(MUSKET, bullet, attacker);
     }
 
     public void discardOnNextTick() {

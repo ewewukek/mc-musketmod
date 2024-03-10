ec6166f121beef209766d808d83f1e2accbb3b34
ewewukek <ewewukek@gmail.com>
2024-03-10 19:37:56 +0300

    copy fix from 2e2a6684bec2b92971739e0134a983737072229d

--- src/main/java/ewewukek/musketmod/BulletEntity.java
+++ src/main/java/ewewukek/musketmod/BulletEntity.java
@@ -3,9 +3,9 @@ package ewewukek.musketmod;
 import java.util.Optional;
 
 import net.minecraft.core.Direction;
-import net.minecraft.core.Holder;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
@@ -13,6 +13,8 @@ import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
@@ -33,7 +35,7 @@ import net.minecraft.world.phys.Vec3;
 public class BulletEntity extends AbstractHurtingProjectile {
     public static final EntityDataAccessor<Float> INITIAL_SPEED = SynchedEntityData.defineId(BulletEntity.class, EntityDataSerializers.FLOAT);
 
-    public static final DamageType BULLET_DAMAGE = new DamageType(MusketMod.MODID + ".bullet", 0.5f);
+    public static final ResourceKey<DamageType> BULLET_DAMAGE = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(MusketMod.MODID, "bullet"));
 
     public static final double MIN_DAMAGE = 0.5;
     public static final double GRAVITY = 0.05;
@@ -63,7 +65,7 @@ public class BulletEntity extends AbstractHurtingProjectile {
     }
 
     public DamageSource causeMusketDamage(BulletEntity bullet, Entity attacker) {
-        return new DamageSource(Holder.direct(BULLET_DAMAGE), bullet, attacker);
+        return level().damageSources().source(BULLET_DAMAGE, bullet, attacker);
     }
 
     public void discardOnNextTick() {

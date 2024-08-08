597872aaa98f5678eeeabfd3c74b877d4db010a6
ewewukek <ewewukek@gmail.com>
2024-08-08 04:54:40 +0300

    replace drop for skeleton with musket

---
+++ src/main/java/ewewukek/musketmod/mixin/EntityMixin.java
@@ -0,0 +1,29 @@
+package ewewukek.musketmod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import ewewukek.musketmod.GunItem;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.AbstractSkeleton;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    @Inject(
+        method = "spawnAtLocation(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;",
+        at = @At("HEAD"), cancellable = true)
+    void dropFromLootTable(ItemStack stack, CallbackInfoReturnable<ItemEntity> ci) {
+        Object object = this;
+        if (stack.getItem() == Items.ARROW
+        && (object instanceof AbstractSkeleton entity) && GunItem.isHoldingGun(entity)) {
+            ItemStack cartridges = new ItemStack(ewewukek.musketmod.Items.CARTRIDGE, stack.getCount());
+            ci.setReturnValue(entity.spawnAtLocation(cartridges, 0.0F));
+            ci.cancel();
+        }
+    }
+}

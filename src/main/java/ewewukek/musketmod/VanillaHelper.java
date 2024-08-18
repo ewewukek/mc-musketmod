cc62a2c73bf454ef10edaa278e998e47a78b72e4
ewewukek <ewewukek@gmail.com>
2024-08-18 21:26:56 +0300

    dynamic lookup for enchantment compatibility

--- src/main/java/ewewukek/musketmod/VanillaHelper.java
+++ src/main/java/ewewukek/musketmod/VanillaHelper.java
@@ -15,12 +15,6 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 
 public class VanillaHelper {
-    public static final TagKey<Item> ENCHANTABLE_KNOCKBACK = TagKey.create(Registries.ITEM, MusketMod.resource("enchantable/knockback"));
-    public static final TagKey<Item> ENCHANTABLE_POWER = TagKey.create(Registries.ITEM, MusketMod.resource("enchantable/power"));
-    public static final TagKey<Item> ENCHANTABLE_SWORD_DAMAGE = TagKey.create(Registries.ITEM, MusketMod.resource("enchantable/sword_damage"));
-    public static final TagKey<Item> ENCHANTABLE_FLAME = TagKey.create(Registries.ITEM, MusketMod.resource("enchantable/flame"));
-    public static final TagKey<Item> ENCHANTABLE_QUICK_CHARGE = TagKey.create(Registries.ITEM, MusketMod.resource("enchantable/quick_charge"));
-
     public static void modifyLootTableItems(ResourceLocation location, LootContext context, Consumer<ItemStack> adder) {
         if (location.getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE)) {
             ResourceKey<LootTable> key = ResourceKey.create(Registries.LOOT_TABLE,
@@ -33,22 +27,13 @@ public class VanillaHelper {
     }
 
     public static boolean isPrimaryEnchantmentFor(Holder<Enchantment> enchantment, ItemStack stack) {
-        if (enchantment.is(Enchantments.KNOCKBACK)) {
-            return stack.is(ENCHANTABLE_KNOCKBACK);
-        }
-        if (enchantment.is(Enchantments.POWER)) {
-            return stack.is(ENCHANTABLE_POWER);
-        }
-        if (enchantment.is(Enchantments.SHARPNESS)
-        || enchantment.is(Enchantments.SMITE)
-        || enchantment.is(Enchantments.BANE_OF_ARTHROPODS)) {
-            return stack.is(ENCHANTABLE_SWORD_DAMAGE);
-        }
-        if (enchantment.is(Enchantments.FLAME)) {
-            return stack.is(ENCHANTABLE_FLAME);
-        }
-        if (enchantment.is(Enchantments.QUICK_CHARGE)) {
-            return stack.is(ENCHANTABLE_QUICK_CHARGE);
+        if (enchantment.kind() == Holder.Kind.REFERENCE) {
+            ResourceKey<Enchantment> key = enchantment.unwrapKey().get();
+            if (key.location().getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE)) {
+                String tagPath = "enchantable/" + key.location().getPath();
+                TagKey<Item> tag = TagKey.create(Registries.ITEM, MusketMod.resource(tagPath));
+                return stack.is(tag);
+            }
         }
         return false;
     }

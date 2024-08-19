00e2e681d8e90bec469c764db1574a4de124bb80
ewewukek <ewewukek@gmail.com>
2024-08-19 20:57:29 +0300

    auto release reloading and prevent auto fire

--- src/main/java/ewewukek/musketmod/mixin/MinecraftMixin.java
+++ src/main/java/ewewukek/musketmod/mixin/MinecraftMixin.java
@@ -19,22 +19,29 @@ import net.minecraft.world.item.ItemStack;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
+    private static boolean useKeyReleased = true;
+
     @SuppressWarnings("resource")
     @Redirect(method = "startUseItem", at = @At(value = "INVOKE",
     target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;useItem(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;"))
-    private InteractionResult handleKeyUseDown(MultiPlayerGameMode gameMode, Player player, InteractionHand hand) {
+    private InteractionResult onUseItem(MultiPlayerGameMode gameMode, Player player, InteractionHand hand) {
         Minecraft client = (Minecraft)(Object)this;
         ItemStack stack = player.getItemInHand(hand);
-        if (client.options.getCameraType().isFirstPerson()
-        && stack.getItem() == Items.MUSKET_WITH_SCOPE && GunItem.isReady(stack)) {
-            setScoping(client, true);
-            if (client.options.keyAttack.isDown()) {
-                return gameMode.useItem(player, hand);
+        if (stack.getItem() instanceof GunItem && GunItem.isReady(stack)) {
+            if (stack.getItem() == Items.MUSKET_WITH_SCOPE
+            && client.options.getCameraType().isFirstPerson()) {
+                useKeyReleased = false;
+                setScoping(client, true);
+                if (client.options.keyAttack.isDown()) {
+                    ScopedMusketItem.recoilTicks = ScopedMusketItem.RECOIL_TICKS;
+                    return gameMode.useItem(player, hand);
+                }
             }
-        } else if (!ScopedMusketItem.isScoping) {
-            return gameMode.useItem(player, hand);
+            if (!useKeyReleased) return InteractionResult.FAIL;
         }
-        return InteractionResult.FAIL;
+        if (ScopedMusketItem.isScoping) return InteractionResult.FAIL;
+        useKeyReleased = false;
+        return gameMode.useItem(player, hand);
     }
 
     @Redirect(method = "handleKeybinds", at = @At(value = "INVOKE",
@@ -43,7 +50,6 @@ public class MinecraftMixin {
         ItemStack stack = client.player.getMainHandItem();
         if (stack.getItem() == Items.MUSKET_WITH_SCOPE && ScopedMusketItem.isScoping) {
             client.startUseItem();
-            ScopedMusketItem.recoilTicks = ScopedMusketItem.RECOIL_TICKS;
         } else {
             return client.startAttack();
         }
@@ -54,10 +60,13 @@ public class MinecraftMixin {
     private void finishReloading(CallbackInfo ci) {
         @SuppressWarnings("resource")
         Minecraft client = (Minecraft)(Object)this;
-        if (client.options.getCameraType().isFirstPerson() && client.player.isUsingItem()) {
+        if (client.player.isUsingItem()) {
             ItemStack stack = client.player.getUseItem();
-            if (stack.getItem() == Items.MUSKET_WITH_SCOPE && GunItem.isLoaded(stack)
-            && client.player.getTicksUsingItem() >= GunItem.reloadDuration(stack) + 10) {
+            int delay = stack.getItem() == Items.MUSKET_WITH_SCOPE
+                && client.options.getCameraType().isFirstPerson()
+                ? 10 : 5;
+            if (stack.getItem() instanceof GunItem && GunItem.isLoaded(stack)
+            && client.player.getTicksUsingItem() >= GunItem.reloadDuration(stack) + delay) {
                 client.gameMode.releaseUsingItem(client.player);
             }
         }
@@ -72,6 +81,7 @@ public class MinecraftMixin {
         || !GunItem.isReady(stack) && !client.options.keyAttack.isDown()) {
             setScoping(client, false);
         }
+        if (!client.options.keyUse.isDown()) useKeyReleased = true;
     }
 
     private static void setScoping(Minecraft client, boolean scoping) {

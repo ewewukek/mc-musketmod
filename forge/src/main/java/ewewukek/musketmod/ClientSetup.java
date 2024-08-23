95946f48aa1065af3fb5ee7fc5c5b5ee23a59487
ewewukek <ewewukek@gmail.com>
2024-08-23 22:22:12 +0300

    cloth config for forge

--- forge/src/main/java/ewewukek/musketmod/ClientSetup.java
+++ forge/src/main/java/ewewukek/musketmod/ClientSetup.java
@@ -9,16 +9,23 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientSetup {
     public ClientSetup(IEventBus bus) {
+        ModLoadingContext.get().registerExtensionPoint(
+            ConfigScreenHandler.ConfigScreenFactory.class,
+            () -> new ConfigScreenHandler.ConfigScreenFactory(
+                (client, parent) -> ClothConfigScreen.build(parent)));
+
         bus.addListener(this::setup);
         bus.addListener(this::registerRenderers);
         MinecraftForge.EVENT_BUS.addListener(EventPriority.LOW, this::renderHand);

72c8db48ba0eb19e708952b19ca2d850efb1e748
ewewukek <ewewukek@gmail.com>
2024-08-28 00:18:20 +0300

    update monster gun animation

--- src/main/java/ewewukek/musketmod/ClientUtilities.java
+++ src/main/java/ewewukek/musketmod/ClientUtilities.java
@@ -16,6 +16,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
@@ -33,7 +34,7 @@ public class ClientUtilities {
     }
 
     public static void poseArm(LivingEntity entity, ModelPart rightArm, ModelPart leftArm, ModelPart head, boolean isRight) {
-        if (entity.isUsingItem()) {
+        if (entity.isUsingItem() || (entity instanceof Mob mob && !mob.isAggressive())) {
             return;
         }
 

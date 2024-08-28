36ea95ff8a10d60b9384821ca666e8df05efcb49
ewewukek <ewewukek@gmail.com>
2024-08-28 03:25:05 +0300

    remove unused import

--- forge/src/main/java/ewewukek/musketmod/ModLootModifier.java
+++ forge/src/main/java/ewewukek/musketmod/ModLootModifier.java
@@ -6,10 +6,8 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;

43ebac938747a6110035b5a8e36eccb0b86a7aba
ewewukek <ewewukek@gmail.com>
2024-08-30 17:38:30 +0300

    attach to RETURN instead of copying behavior

--- fabric/src/main/java/ewewukek/musketmod/mixin/LootDataManagerMixin.java
+++ fabric/src/main/java/ewewukek/musketmod/mixin/LootDataManagerMixin.java
@@ -1,28 +1,30 @@
 package ewewukek.musketmod.mixin;
 
-import java.util.Map;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import ewewukek.musketmod.ILootTableId;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootDataId;
 import net.minecraft.world.level.storage.loot.LootDataManager;
 import net.minecraft.world.level.storage.loot.LootTable;
 
 @Mixin(LootDataManager.class)
 abstract class LootDataManagerMixin {
-    @Shadow
-    private Map<LootDataId<?>, ?> elements;
+    private ResourceLocation location;
 
     @Inject(method = "getElement", at = @At("HEAD"))
-    private void getElement(LootDataId<LootTable> id, CallbackInfoReturnable<LootTable> ci) {
-        Object table = elements.get(id);
-        if (table instanceof LootTable) {
-            ((ILootTableId)table).setLocation(id.location());
+    private void getElementHead(LootDataId<LootTable> id, CallbackInfoReturnable<LootTable> ci) {
+        location = id.location();
+    }
+
+    @Inject(method = "getElement", at = @At("RETURN"))
+    private void getElement(CallbackInfoReturnable<LootTable> ci) {
+        LootTable table = ci.getReturnValue();
+        if (table != null) {
+            ((ILootTableId)table).setLocation(location);
         }
     }
 }

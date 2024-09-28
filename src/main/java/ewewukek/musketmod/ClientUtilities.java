902390aa75ddc6331aaf04c775d8070e6fe97eb2
ewewukek <ewewukek@gmail.com>
2024-09-28 12:24:08 +0300

    fix NPE in ClientUtilities.poseArm with Twilight Forest (#46)

--- src/main/java/ewewukek/musketmod/ClientUtilities.java
+++ src/main/java/ewewukek/musketmod/ClientUtilities.java
@@ -62,7 +62,7 @@ public class ClientUtilities {
     }
 
     public static boolean poseArm(LivingEntity entity, ModelPart arm, ModelPart head, boolean isRight) {
-        if (entity.isUsingItem() || (entity instanceof Mob mob && !mob.isAggressive())) {
+        if (entity == null || entity.isUsingItem() || (entity instanceof Mob mob && !mob.isAggressive())) {
             return false;
         }
 
